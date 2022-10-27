3fdff8cea1f095ce33b45406ef72badab270fd6d
satpreet234 <singhsatpreet13@gmail.com>
2022-10-27 12:37:11 +0530

    heroku commit

--- connection/connection.js
+++ connection/connection.js
@@ -1,12 +1,12 @@
 const mongoose=require('mongoose');
 
 var connect=()=>{
-    mongoose.connect('mongodb://localhost:27017/usersdb',{
+    mongoose.connect('mongodb+srv://root:root@cluster0.rhtxjhj.mongodb.net/?retryWrites=true&w=majority',{
         useNewUrlParser: true,
         useUnifiedTopology: true
       },(err,success)=>{
         if(err) throw err;
-        else{  console.log('database connected successfully');}
+        else{  console.log('database connected 1 successfully');}
 
     })
 }
