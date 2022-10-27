d05986ee88ee8d59bbd5c9d5fee2dfee9cd94d56
satpreet234 <singhsatpreet13@gmail.com>
2022-10-27 13:15:37 +0530

    heroku commit

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express()
-const port = 3010;
+const port = process.env.PORT || 3010;
 const cors=require('cors');
 app.use(cors())
 const dotenv=require('dotenv');
